fad0122500ce703411c5d06f9c463dc55f9370c0
Paweł <pawel_gnat@o2.pl>
2023-05-03 19:15:52 +0200

    feat: add initial global vars, fonts, colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Space_Grotesk } from 'next/font/google';
+
+const fonts = Space_Grotesk({
+  weight: ['300', '400', '600'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${fonts.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
