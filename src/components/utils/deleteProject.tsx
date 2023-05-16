c57dd167e327b183e69656c5a4e62987cc711f5a
Paweł <pawel_gnat@o2.pl>
2023-05-16 20:18:12 +0200

    feat: add delete project option inside ustawienia page

---
+++ src/components/utils/deleteProject.tsx
@@ -0,0 +1,6 @@
+import { Project } from '@/types/types';
+import { getProjectsFromLocalStorage } from './localStorageDatabase';
+
+export const deleteProject = async (project: Project) => {
+  //   const existingProjects = await getProjectsFromLocalStorage();
+};
--- src/pages/kreator/[projektid]/ustawienia.tsx
+++ src/pages/kreator/[projektid]/ustawienia.tsx
@@ -1,13 +1,38 @@
 import { ProjectLayout } from '@/components/layout/ProjectLayout/ProjectLayout';
+import {
+  getProjectsFromLocalStorage,
+  setProjectsToLocalStorage,
+} from '@/components/utils/localStorageDatabase';
 import { useProject } from '@/hooks/useProject';
+import UserContext from '@/store/user-context';
+import { Project } from '@/types/types';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
 
 export default function UstawieniaPage() {
-  const project = useProject();
+  const project = useProject()!;
+  const router = useRouter();
+  const context = useContext(UserContext);
+
+  async function deleteProject(project: Project) {
+    const existingProjects = await getProjectsFromLocalStorage();
+    const filteredProjects = existingProjects.filter(
+      (existingProject: Project) =>
+        JSON.stringify(existingProject) !== JSON.stringify(project),
+    );
+
+    if (filteredProjects) {
+      await setProjectsToLocalStorage(filteredProjects);
+      context.setProjects();
+      router.push('/kreator');
+    }
+  }
 
   return (
     <ProjectLayout>
       <p>UstawieniaPage</p>
       {project && <p>{project.name}</p>}
+      <button onClick={(event) => deleteProject(project)}>Usuń projekt</button>
     </ProjectLayout>
   );
 }
