7f9563f345be70342754e99ef8c1132b2ca116f3
Paweł <pawel_gnat@o2.pl>
2023-05-24 20:32:45 +0200

    feat: create logo

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link';
 import styles from './Navbar.module.css';
 import stylesLink from '../../ui/Link/Link.module.css';
+import { Logo } from '@/assets/Logo/Logo';
 
 export default function Navbar() {
   return (
     <header>
       <nav className={styles.nav}>
-        <div>
-          <Link href="/" className={stylesLink.navlink}>
-            Kosztomiar
+        <div className={stylesLink['logo-container']}>
+          <Link href="/" className={stylesLink.logo}>
+            <Logo /> Kosztomiar
           </Link>
         </div>
         <div>
