2345bc44b879695e8f0c9421e6a1f7cf579595b9
Paweł <pawel_gnat@o2.pl>
2023-06-02 20:16:32 +0200

    feat: add pdf renderer library, create podglad pdf component

--- src/pages/kreator/[projektid]/podglad.tsx
+++ src/pages/kreator/[projektid]/podglad.tsx
@@ -1,12 +1,27 @@
+import stylesButton from '../../../components/ui/Button/Button.module.css';
 import { ProjectLayout } from '@/components/layout/ProjectLayout/ProjectLayout';
+import { PDFDocument } from '@/components/pdf/PDFDocument';
 import { useProject } from '@/hooks/useProject';
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 
 export default function PodgladPage() {
-  const project = useProject();
+  const project = useProject()!;
 
   return (
     <ProjectLayout>
-      <p>PodgladPage</p>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <PDFViewer style={{ maxWidth: 1000, aspectRatio: '1/1' }} width={'100%'}>
+          <PDFDocument data={project} />
+        </PDFViewer>
+
+        <PDFDownloadLink
+          document={<PDFDocument data={project} />}
+          fileName={project && project.name}
+          className={stylesButton.button}
+        >
+          Pobierz w PDF
+        </PDFDownloadLink>
+      </div>
     </ProjectLayout>
   );
 }
