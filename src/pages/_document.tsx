8ff20eb2a3e70da0c9c28fd53f54edd20b51f553
Paweł <pawel_gnat@o2.pl>
2023-07-06 19:03:10 +0200

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="pl">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </Head>
       <body>
         <Main />
         <div id="portal"></div>
