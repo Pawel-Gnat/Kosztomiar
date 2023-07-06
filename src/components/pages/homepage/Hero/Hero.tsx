67baab0dfd9d6d0be41a7dc087275ca341359db9
Paweł <pawel_gnat@o2.pl>
2023-07-06 21:38:22 +0200

    feat: add AOS library

--- src/components/pages/homepage/Hero/Hero.tsx
+++ src/components/pages/homepage/Hero/Hero.tsx
@@ -8,15 +8,17 @@ export const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles['text-container']}>
-        <h1 className={styles.heading}>
+        <h1 className={styles.heading} data-aos="fade-up" data-aos-delay="300">
           Twórz raporty, które przekształcają Twoje dane w wartość biznesową
         </h1>
-        <Text
-          content="Niezależnie od branży, aplikacja zapewnia narzędzia do efektywnego
+        <div data-aos="fade-up" data-aos-delay="400">
+          <Text
+            content="Niezależnie od branży, aplikacja zapewnia narzędzia do efektywnego
           tworzenia i prezentowania kosztów, co pomaga w podejmowaniu mądrych decyzji
           biznesowych."
-        />
-        <div className={styles.links}>
+          />
+        </div>
+        <div className={styles.links} data-aos="fade-up" data-aos-delay="500">
           <Link href="/kreator" className={stylesLink.navlink}>
             Wypróbuj
           </Link>
@@ -33,6 +35,8 @@ export const Hero = () => {
         aria-hidden={true}
         className={styles.image}
         priority
+        data-aos="fade-up"
+        data-aos-delay="200"
       />
     </section>
   );
